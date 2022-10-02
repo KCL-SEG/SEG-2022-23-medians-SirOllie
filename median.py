636f79d9559ce511965182b69485a8b67355d8d4
Oliver Singer <owsinger@outlook.com>
2022-10-02 23:54:36 +0100

    fixed error where variable numbers would be made empty.

--- median.py
+++ median.py
@@ -9,9 +9,9 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-numbers = numbers.sort()
-numberslength = int(len(numbers))
+sortednumbers = sorted(numbers, key = float)
+numberslength = int(len(sortednumbers))
 if numberslength % 2 == 1: #checks if the length of numbers is odd
-    print(numbers[int((numberslength/2)-0.5)])
+    print(sortednumbers[int((numberslength/2)-0.5)])
 else:
-    print((numbers[int((numberslength/2)-1)]+numbers[int(numberslength/2)])/2)
+    print((sortednumbers[int((numberslength/2)-1)]+sortednumbers[int(numberslength/2)])/2)
