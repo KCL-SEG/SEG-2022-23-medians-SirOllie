9ea8a31dee793a435f607aa76b3d0886af9a4c21
Oliver Singer <owsinger@outlook.com>
2022-10-02 23:37:57 +0100

    median.py updated to sort the list of numbers in order.

--- median.py
+++ median.py
@@ -9,7 +9,7 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+numbers.sort()
 numberslength = int(len(numbers))
 if numberslength % 2 == 1: #checks if the length of numbers is odd
     print(numbers[int((numberslength/2)-0.5)])
